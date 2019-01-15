e90719204a43457e0c68c2fa6d397b3e88ed2c9b
shuunz <shuunz120@gmail.com>
2019-01-15 15:25:46 +0300

    sign up

--- lost-and-found-react/src/components/SignUp.js
+++ lost-and-found-react/src/components/SignUp.js
@@ -2,37 +2,59 @@ import React, { Component } from 'react';
 
 
 class SignUp extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+          username :'',
+          email : '',
+          phone : ''
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
    
     handleSubmit(event){
         event.preventDefault();
-        const data = new FormData(event.target);
+        const data = {
+          username : this.state.username,
+          email : this.state.email,
+          phone : this.state.phone
+        }
 
+
+        console.log("data\n\n\n\n\n\n ******" , data)
         const url = 'http://localhost:3000/users'
         fetch(url, {
             method: 'POST',
             headers: {
               "Content-Type": "application/json"
             },
-            body: data
+            body: JSON.stringify(data)
           })
-          .then(response => response.json())
+          // .then(response => response.json())
           .then(data => {
             console.log(data)
             console.log("Success"); 
           })
     }
+
+    handleChange(event) {
+      console.log("changes"); 
+
+      let formData = {}; 
+      formData[event.target.name] = event.target.value ; 
+   
+      this.setState(formData);
+    }
    
     render(){
         return(
             <div>
-            <form className="show-form" onSubmit={this.handleSubmit()}>
-            <label>Name:</label><input type="text"  name="name" /><br/>
-              <label>Email:</label><input type="text" name="email" /><br/>
-              <label>Phone:</label><input type="text" name="phone" /><br/>
+              <h3>Sign Up</h3>
+            <form onSubmit={this.handleSubmit.bind(this)}>
+            <label>Name:</label><input type="text" onChange={this.handleChange.bind(this)} name="username" /><br/>
+            
+              <label>Email:</label><input type="text" onChange={this.handleChange.bind(this)}  name="email" /><br/>
+              <label>Phone:</label><input type="text" onChange={this.handleChange.bind(this)}  name="phone" /><br/>
               <button>submit</button>
             </form>
           </div>
